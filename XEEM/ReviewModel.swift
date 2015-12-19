c4f57fb10a990cdef23e98d8ed28f0492f8c4af1
Le Thanh Tan <thanhtan1105@gmail.com>
2015-12-19 17:46:13 +0700

    [Tan] create api get Service with current location

---
+++ XEEM/ReviewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewModel.swift
+//  XEEM
+//
+//  Created by Le Thanh Tan on 12/19/15.
+//  Copyright © 2015 JadeLe. All rights reserved.
+//
+
+import UIKit
+
+class ReviewModel: NSObject {
+
+}
---
+++ XEEM/ShopModel.swift
@@ -0,0 +1,13 @@
+//
+//  ShopModel.swift
+//  XEEM
+//
+//  Created by Le Thanh Tan on 12/19/15.
+//  Copyright © 2015 JadeLe. All rights reserved.
+//
+
+import UIKit
+
+class ShopModel: NSObject {
+
+}
