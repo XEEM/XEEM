4e6658914bc3eb6be39d2e21e08a56fa1eba0628
Jadele511 <baongocle1991@gmail.com>
2015-12-20 15:24:01 +0700

    Login and Setting screens

---
+++ XEEM/CurrencyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyTableViewCell.swift
+//  XEEM
+//
+//  Created by USER on 12/19/15.
+//  Copyright © 2015 JadeLe. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ XEEM/PaymentMethodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaymentMethodTableViewCell.swift
+//  XEEM
+//
+//  Created by USER on 12/17/15.
+//  Copyright © 2015 JadeLe. All rights reserved.
+//
+
+import UIKit
+
+class PaymentMethodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
